77d2c69e6368d4f0a6454e614c81cfdd15e30a38
Stefanus Andre <stefanusandre07@gmail.com>
2025-06-03 15:24:24 +0700

    perubahan untuk image index

--- src/Index.jsx
+++ src/Index.jsx
@@ -13,7 +13,7 @@ export default function Index() {
                     <h1 className="mb-4 text-4xl font-exltrabold tracking-tight leading-none text-white md:text-5xl lg:text-6xl">We invest in the world’s potential</h1>\
                     <p className="mb-8 text-lg font-normal text-gray-300 md:text-5xl lg:text-6xl">Here at Flowbite we focus on markets where technology, innovation, and capital can unlock long-term value and drive economic growth.</p>
                     <div className="flex flex-col space-y-4 sm:flex-row sm:justify-center sm:space-y-0">
-                      <a href="#" className="inline-flex justify-center items-center py-3 px-5 text-base font-medium text-center text-white rounded-lg bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 dark:focus:ring-blue-900">
+                        <a href="#" className="inline-flex justify-center items-center py-3 px-5 text-base font-medium text-center text-white rounded-lg bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 dark:focus:ring-blue-900">
                 Get started
                         <svg className="w-3.5 h-3.5 ms-2 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
                             <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
@@ -22,7 +22,6 @@ export default function Index() {
                     </div>
                 </div>
             </section>
-
 
         {/* hero 1 section  */}
         <section class="bg-white dark:bg-gray-900">
@@ -44,7 +43,37 @@ export default function Index() {
         </div>
         </section>
 
+           {/* hero 2 section  */}
+            <section class="bg-white dark:bg-gray-900">
+                <div class="grid max-w-screen-xl px-4 py-8 mx-auto lg:grid-cols-12 lg:gap-8 xl:gap-0 lg:py-16">
+                    
+                    <div class="hidden lg:col-span-5 lg:flex items-center justify-center">
+                    <img src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/hero/phone-mockup.png" alt="mockup" />
+                    </div>
+                    
+                    <div class="lg:col-span-7 flex flex-col justify-center">
+                    <h1 class="max-w-2xl mb-4 text-4xl font-extrabold tracking-tight leading-none md:text-5xl xl:text-6xl dark:text-white">
+                        Payments tool for software companies
+                    </h1>
+                    <p class="max-w-2xl mb-6 font-light text-gray-500 lg:mb-8 md:text-lg lg:text-xl dark:text-gray-400">
+                        From checkout to global sales tax compliance, companies around the world use Flowbite to simplify their payment stack.
+                    </p>
+                    <div class="flex space-x-4">
+                        <a href="#" class="inline-flex items-center justify-center px-5 py-3 text-base font-medium text-white bg-primary-700 rounded-lg hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 dark:focus:ring-primary-900">
+                        Get started
+                        <svg class="w-5 h-5 ml-2 -mr-1" fill="currentColor" viewBox="0 0 20 20">
+                            <path fill-rule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clip-rule="evenodd"/>
+                        </svg>
+                        </a>
+                        <a href="#" class="inline-flex items-center justify-center px-5 py-3 text-base font-medium text-gray-900 border border-gray-300 rounded-lg hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 dark:text-white dark:border-gray-700 dark:hover:bg-gray-700 dark:focus:ring-gray-800">
+                        Speak to Sales
+                        </a>
+                    </div>
+                    </div>
+
+                </div>
+                </section>
         <Footer/>
     </>
-   )
+)
 }
